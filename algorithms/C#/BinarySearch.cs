80f60f2b0d29bb4bd5664f6d257d9da61fba514a
Justin Braben <jbraben@dmsiworks.com>
2022-08-02 20:47:53 -0600

    Added - 704. Binary Search - question

---
+++ algorithms/C#/BinarySearch.cs
@@ -0,0 +1,23 @@
+public class Solution
+{
+    public int Search(int[] nums, int target)
+    {
+        int pivot = 0;
+        int left = 0;
+        int right = nums.Length - 1;
+        while (left <= right)
+        {
+
+            pivot = left + (right - left) / 2;
+
+            if (nums[pivot] == target)
+                return pivot;
+
+            if (target < nums[pivot])
+                right = pivot - 1;
+            else
+                left = pivot + 1;
+        }
+        return -1;
+    }
+}

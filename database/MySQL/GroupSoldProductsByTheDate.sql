b4a50208d4a83a747ff9200e324b14702378e84a
Justin Braben <jbraben@dmsiworks.com>
2022-08-05 10:34:18 -0600

    Fix comment format

--- database/MySQL/GroupSoldProductsByTheDate.sql
+++ database/MySQL/GroupSoldProductsByTheDate.sql
@@ -1,8 +1,10 @@
-# query for sell_date
-# group all items in query by the sell_date (to find all items sold on the same day)
-# Order all items by sell_date
-# Find and count the number  of  distinct products (sold on the same day)
-# Group all the products sold on the same day and display it
+/* 
+query for sell_date                                                                  
+group all items in query by the sell_date (to find all items sold on the same day)   
+Order all items by sell_date                                                         
+Find and count the number  of  distinct products (sold on the same day)              
+Group all the products sold on the same day and display it                           
+*/
 SELECT
     sell_date,
     COUNT(DISTINCT(product)) AS num_sold,

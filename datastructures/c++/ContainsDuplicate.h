8343996ece11089d2a88bb85c02aae4509b49c96
Justin Braben <jbraben@dmsiworks.com>
2022-08-18 09:08:57 -0600

    Added - 1. Two Sum - question (datastructures method)

--- datastructures/c++/ContainsDuplicate.h
+++ datastructures/c++/ContainsDuplicate.h
@@ -2,11 +2,15 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         unordered_map<int, bool> mp;
+
         for (auto x : nums) {
+
             if (mp[x])
                 return true;
+
             mp[x] = true;
         }
+
         return false;
     }
 };
--- datastructures/c++/MaximumSubarray.h
+++ datastructures/c++/MaximumSubarray.h
@@ -4,8 +4,10 @@ public:
         int sum = 0, res = INT_MIN;
 
         for (auto& x : nums) {
+
             sum = max(sum + x, x);
             res = max(res, sum);
+
         }
 
         return res;

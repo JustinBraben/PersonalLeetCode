ccf8b3cde0473e292fc9ca308b13495c3d013e64
Justin Braben <jbraben@dmsiworks.com>
2022-09-12 08:22:24 -0600

    Added - 553. Optimal Division - question

---
+++ algorithms/C++/OptimalDivision.h
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string optimalDivision(vector<int>& nums) {
+        int n = nums.size();
+
+        // check for size if its 1 then we can't use parantheses and 
+        // if its 2 then also we should not use it.
+
+        if (n == 1) return to_string(nums[0]);
+        if (n == 2) return to_string(nums[0]) + "/" + to_string(nums[1]);
+
+        //  for size greater than 2 add paranthese after first number 
+
+        string ans = to_string(nums[0]);
+        ans.append("/(");
+        for (int i = 1; i < n - 1; i++) {
+            ans.append(to_string(nums[i]) + "/");
+        }
+        ans.append(to_string(nums[n - 1]));
+        ans.append(")");
+
+        // finally this becomes as a/(b/c/d/....) which is our answer
+
+        return ans;
+    }
+};

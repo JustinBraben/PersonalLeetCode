69cfc972ae75cf5f99f4e8946f633d4e13c945a8
Justin Braben <jbraben@dmsiworks.com>
2023-04-16 20:57:07 -0600

    Updated main and gitignore

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "algorithms/C++/RemovingStarsFromAString.h" // Include the header file for LeetCode question 1
+
+int main() {
+    // Test solution for LeetCode question 1
+    std::vector<int> nums = { 2, 7, 11, 15 };
+    int target = 9;
+    std::vector<int> result = twoSum(nums, target);
+    std::cout << "Indices: " << result[0] << ", " << result[1] << std::endl;
+
+    // Test solution for LeetCode question 2
+    // ...
+
+    // Test solution for LeetCode question 3
+    // ...
+
+    // ...
+    return 0;
+}
